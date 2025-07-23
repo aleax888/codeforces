2a741c95fafb67ea0c15fba1b5c69a793ba895ec
aleax <alex.maglio.neyra.herrada@gmail.com>
2025-07-22 20:15:30 -0500

    update auto (create script)

--- create_script.sh
+++ create_script.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# are there 2 arguments?
+# Are there 2 arguments?
 if [ "$#" -ne 2 ]; then
-  echo "Use: $0 <nombre_carpeta> <nombre_archivo>"
+  echo "Uso: $0 <nombre_carpeta> <nombre_archivo>"
   exit 1
 fi
 
 FOLDER_NAME="$1"
 SCRIPT_NAME="$2"
 
-# create folder if it doesn't exist
+# Replace invalid Windows characters in the script name
+SCRIPT_NAME_SAFE=$(echo "$SCRIPT_NAME" | sed 's/[<>:"\/\\|?*]/_/g')
+
+# Create folder if it doesn't exist
 mkdir -p "$FOLDER_NAME"
 
-# copy script template into new script
-cat template.txt >> "${FOLDER_NAME}/${SCRIPT_NAME}.cpp"
+# Copy script template into new script
+cat template.txt >> "${FOLDER_NAME}/${SCRIPT_NAME_SAFE}.cpp"
 
-# open new script
-code "${FOLDER_NAME}/${SCRIPT_NAME}.cpp"
+# Open new script
+code "${FOLDER_NAME}/${SCRIPT_NAME_SAFE}.cpp"
