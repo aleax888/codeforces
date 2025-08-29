7b6ea36fd47576ad649e9a6492d4d4215e527ad9
aleax <alex.maglio.neyra.herrada@gmail.com>
2025-08-29 11:25:44 -0500

    update template.cpp
    Convert template.txt to template.cpp and write base code for problems with several test cases

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, ans;
+    cin >> t;
+    for (size_t test_case_i = 0; test_case_i < t; test_case_i++)
+    {
+        // code
+        cout << ans;
+    }
+}
+
+// << link >>
